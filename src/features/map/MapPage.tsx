e6c65ab71b09fd21c2aa77af032b01bae1220cee
mia2583 <mia2583@pusan.ac.kr>
2024-12-14 15:31:42 +0900

    [feat] 구글 맵 연결
    구글 맵 api 설정
    구글 지도 화면에 보이기

--- src/features/map/MapPage.tsx
+++ src/features/map/MapPage.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {StyleSheet} from 'react-native';
+import MapView from 'react-native-maps';
 
 export default function MapPage() {
   return (
-    <View>
-      <Text>Welcome</Text>
-    </View>
+    <MapView
+      initialRegion={{
+        latitude: 35.91395373474155,
+        longitude: 127.73829440215488,
+        latitudeDelta: 5,
+        longitudeDelta: 5,
+      }}
+      style={[styles.map]}
+      showsUserLocation={true}
+      showsMyLocationButton={true}
+    />
   );
 }
+
+const styles = StyleSheet.create({
+  map: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
+});
